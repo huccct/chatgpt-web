7d39eb954b06c171b3830ff9a9536dd94a91b58b
huccct <2972223145@qq.com>
2023-11-15 16:55:03 +0800

    feat(*): init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
+import { ConfigProvider } from 'antd'
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#202123',
+            colorBgContainer: '#565869'
+          }
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </>
   )
 }
--- src/views/chat/layout/index.tsx
+++ src/views/chat/layout/index.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined
+  PlusOutlined
 } from '@ant-design/icons'
-import { Layout, Menu, Button, theme } from 'antd'
+import { Layout, Menu, Button, theme, Row, Input, Col } from 'antd'
 
+const { TextArea } = Input
 const { Header, Sider, Content } = Layout
 
 const Index: React.FC = () => {
@@ -15,31 +14,29 @@ const Index: React.FC = () => {
   const {
     token: { colorBgContainer }
   } = theme.useToken()
+
   return (
     <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        style={{ height: '100vh', background: '#202123' }}
+      >
+        <Row justify="center" className="mt-[20px]">
+          <Button
+            icon={<PlusOutlined style={{ color: '#fff' }} />}
+            className="w-11/12 border-none bg-[#40414f]"
+            size="large"
+          >
+            {!collapsed && <span className="text-white">新的聊天</span>}
+          </Button>
+        </Row>
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1'
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2'
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3'
-            }
-          ]}
+          items={[]}
         />
       </Sider>
       <Layout>
@@ -57,13 +54,27 @@ const Index: React.FC = () => {
         </Header>
         <Content
           style={{
-            margin: '24px 16px',
+            display: 'flex',
+            justifyContent: 'center',
             padding: 24,
             minHeight: 280,
             background: colorBgContainer
           }}
         >
-          Content
+          <TextArea autoSize bordered={false} readOnly />
+
+          <Row className="fixed bottom-[40px] min-w-[50%] leading-[56px] h-[56px]">
+            <TextArea
+              placeholder="请输入你想发送的内容..."
+              className="rounded-[15px] text-18px"
+              style={{
+                boxShadow:
+                  '0 1px 2px rgba(0,0,0,0.07), 0 2px 4px rgba(0,0,0,0.07), 0 4px 8px rgba(0,0,0,0.07), 0 8px 16px rgba(0,0,0,0.07), 0 16px 32px rgba(0,0,0,0.07), 0 32px 64px rgba(0,0,0,0.07)',
+                resize: 'none'
+              }}
+              bordered={false}
+            />
+          </Row>
         </Content>
       </Layout>
     </Layout>
